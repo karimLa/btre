d4c6a6f19866aa0068406888d4e1b1e335d45def
karim laayoun <karimacc001@gmail.com>
2020-01-20 18:57:17 +0100

    added dashboard functionality

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
+from contacts.models import Contact
+
 
 def login(request):
   if request.method == 'POST':
@@ -67,4 +69,10 @@ def register(request):
 
 
 def dashboard(request):
-  return render(request, 'accounts/dashboard.html')
+  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+  context = {
+    'contacts': user_contacts
+  }
+
+  return render(request, 'accounts/dashboard.html', context)
